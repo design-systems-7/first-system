2a41967b857534d61d2b8460d1c0e7621cb3d8cd
Ilya Kanishev <kanishev2002@gmail.com>
2024-10-25 16:06:30 -0500

    Migrate to httpx

--- backend/app/core/assign_order_service/data_provider.py
+++ backend/app/core/assign_order_service/data_provider.py
@@ -2,7 +2,7 @@ from os import environ
 from typing import Tuple
 from urllib.parse import urlencode
 
-import aiohttp
+import httpx
 import asyncio
 
 from app.schemas.order import OrderData, ZoneData, ExecuterProfile, ConfigMap, TollRoadsData
@@ -14,61 +14,65 @@ class DataProvider:
     async def fetch_order_info(self, order_id: str, executer_id: str) -> Tuple[
         OrderData, ZoneData, ExecuterProfile, ConfigMap, TollRoadsData
     ]:
-        async with aiohttp.ClientSession() as session:
-            order_data_task = self.get_order_data(session, order_id)
-            executer_profile_task = self.get_executer_profile(session, executer_id)
-            configs_task = self.get_configs(session)
+        async with httpx.AsyncClient() as client:
+            order_data_task = self.get_order_data(client, order_id)
+            executer_profile_task = self.get_executer_profile(client, executer_id)
+            configs_task = self.get_configs(client)
 
             order_data, executer_profile, configs = await asyncio.gather(
                 order_data_task, executer_profile_task, configs_task
             )
 
-            zone_data = await self.get_zone_data(session, order_data.zone_id)
+            zone_data = await self.get_zone_data(client, order_data.zone_id)
 
-            toll_roads_data = await self.get_toll_roads(session, zone_data.display_name)
+            toll_roads_data = await self.get_toll_roads(client, zone_data.display_name)
 
             return order_data, zone_data, executer_profile, configs, toll_roads_data
 
-    async def get_order_data(self, session: aiohttp.ClientSession, order_id: str) -> OrderData:
+    async def get_order_data(self, client: httpx.AsyncClient, order_id: str) -> OrderData:
         url = environ.get('order_data_endpoint') + urlencode({'id': order_id})
-        async with session.get(url) as response:
-            response_data = await response.json()
-            return OrderData(
-                id=order_id,
-                zone_id=response_data['zone_id'],
-                user_id=response_data['user_id'],
-                base_coin_amount=response_data['base_coin_amount']
-            )
+        response = await client.get(url)
+        response_data = response.json()
+        return OrderData(
+            id=order_id,
+            zone_id=response_data['zone_id'],
+            user_id=response_data['user_id'],
+            base_coin_amount=response_data['base_coin_amount']
+        )
 
-    async def get_zone_data(self, session: aiohttp.ClientSession, zone_id: str) -> ZoneData:
+
+    async def get_zone_data(self, client: httpx.AsyncClient, zone_id: str) -> ZoneData:
         url = environ.get('zone_data_endpoint') + urlencode({'id': zone_id})
-        async with session.get(url) as response:
-            response_data = await response.json()
-            return ZoneData(
-                id=zone_id,
-                coin_coeff=response_data['coin_coeff'],
-                display_name=response_data['display_name']
-            )
+        response = await client.get(url)
+        response_data = response.json()
+        return ZoneData(
+            id=zone_id,
+            coin_coeff=response_data['coin_coeff'],
+            display_name=response_data['display_name']
+        )
 
-    async def get_executer_profile(self, session: aiohttp.ClientSession, executer_id: str) -> ExecuterProfile:
+
+    async def get_executer_profile(self, client: httpx.AsyncClient, executer_id: str) -> ExecuterProfile:
         url = environ.get('executer_profile_endpoint') + urlencode({'id': executer_id})
-        async with session.get(url) as response:
-            response_data = await response.json()
-            return ExecuterProfile(
-                id=executer_id,
-                tags=response_data['tags'],
-                rating=response_data['rating']
-            )
+        response = await client.get(url)
+        response_data = response.json()
+        return ExecuterProfile(
+            id=executer_id,
+            tags=response_data['tags'],
+            rating=response_data['rating']
+        )
 
-    async def get_configs(self, session: aiohttp.ClientSession) -> ConfigMap:
+
+    async def get_configs(self, client: httpx.AsyncClient) -> ConfigMap:
         url = environ.get('configs_endpoint')
-        async with session.get(url) as response:
-            return ConfigMap(await response.json())
+        response = await client.get(url)
+        return ConfigMap(response.json())
 
-    async def get_toll_roads(self, session: aiohttp.ClientSession, zone_display_name: str) -> TollRoadsData:
+
+    async def get_toll_roads(self, client: httpx.AsyncClient, zone_display_name: str) -> TollRoadsData:
         url = environ.get('tollroads_endpoint') + urlencode({'zone_display_name': zone_display_name})
-        async with session.get(url) as response:
-            response_data = await response.json()
-            return TollRoadsData(
-                response_data['bonus_amount']
-            )
+        response = await client.get(url)
+        response_data = response.json()
+        return TollRoadsData(
+            response_data['bonus_amount']
+        )
