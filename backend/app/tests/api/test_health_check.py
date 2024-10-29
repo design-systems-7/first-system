d09a191d0139df6119d3575ee16d64cf885f76d6
Blinov Ilya <ulkhan.2014@mail.ru>
2024-10-30 00:13:15 +0300

    Add template for tests

---
+++ backend/app/tests/api/test_health_check.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+
+from app.core.config import settings
+
+
+def test_assign_order(
+    client: TestClient
+) -> None:
+    response = client.get(
+        f"{settings.API_V1_STR}/utils/health-check/",
+    )
+    assert response.status_code == 200
+    content = response.text
+    assert content == "true"
--- backend/scripts/tests-start.sh
+++ backend/scripts/tests-start.sh
@@ -1,9 +1,7 @@
 #! /usr/bin/env bash
-set -e
-set -x
 
 python app/backend_pre_start.py
 
-#coverage run --source=app -m pytest
+coverage run --source=app -m pytest
 #coverage report --show-missing
 #coverage html --title "${@-coverage}"
