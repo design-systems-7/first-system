44ea09586a8e0690611c1843cab02713a391dbdb
Dmitrii <sciencenikitin@mail.ru>
2024-11-03 20:52:18 +0300

    Second system funcs are removed

--- backend/app/crud/order.py
+++ backend/app/crud/order.py
@@ -14,35 +14,6 @@ class DatabaseAdapter:
         self.async_session.add(new_order)
         return new_order
 
-    async def get_taken_order(self, session: AsyncSession, executor_id: int) -> Optional[Order]:
-        query = (
-                Order.select()
-                .where(Order.executor_id == executor_id, Order.status == OrderStatus.taken)
-        )
-        result = await session.execute(query)
-        order = result.scalar_one_or_none()
-        return order
-
-    async def get_active_orders_for_execution(self, session: AsyncSession, executor_id: int) -> List[Order]:
-        query = (
-            select(Order)
-            .where(Order.executor_id == executor_id, Order.status == OrderStatus.active)
-        )
-        result = await session.execute(query)
-        orders = result.scalars().all()
-        return orders
-
-    async def take_order(self, session: AsyncSession, order_id: int) -> Optional[Order]:
-        query = (
-            update(Order)
-            .where(Order.order_id == order_id, Order.status == OrderStatus.active)
-            .values(status=OrderStatus.taken)
-            .returning(Order)
-        )
-        result = await session.execute(query)
-        order = result.scalar_one_or_none()
-        return order
-
     async def cancel_active_order_within_safety_time(self,
                                                      session: AsyncSession,
                                                      order_id: int,
@@ -53,17 +24,6 @@ class DatabaseAdapter:
                        Order.status == OrderStatus.active,
                        Order.created_at > safety_datetime)
                 .values(status=OrderStatus.cancelled)
-                .returning(Order)
-        )
-        result = await session.execute(query)
-        order = result.scalar_one_or_none()
-        return order
-
-    async def complete_order(self, session: AsyncSession, order_id: int) -> Optional[Order]:
-        query = (
-                Order.update()
-                .where(Order.order_id == order_id)
-                .values(status=OrderStatus.done)
                 .returning(Order)
         )
         result = await session.execute(query)
