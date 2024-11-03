6f4776e19f40fc236ba8ad412ece4bf70b376556
Blinov Ilya <ulkhan.2014@mail.ru>
2024-11-03 18:14:54 +0300

    Propose crud changes

--- backend/app/core/cancel_order.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-from app.models.order import Order, OrderStatus
-
-
-def can_cancel_order(order: Order) -> bool:
-    return (
-            order.status == OrderStatus.active and
-            order.created_at + timedelta(minutes=10) > datetime.utcnow()
-    )
