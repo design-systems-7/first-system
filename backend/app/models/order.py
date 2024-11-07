546938befd747cbfece41b21a8510f8bd36cee2a
Dmitrii <sciencenikitin@mail.ru>
2024-11-07 19:14:55 +0300

    assigned_order_id is added; INT changed for UUID

--- backend/app/models/order.py
+++ backend/app/models/order.py
@@ -1,7 +1,9 @@
 from app.models.base_class import Base
 from sqlalchemy import Column, Integer, String, Float, DateTime, Enum, JSON
+from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
 import enum
+import uuid
 
 
 class OrderStatus(enum.Enum):
@@ -12,10 +14,13 @@ class OrderStatus(enum.Enum):
 
 
 class Order(Base):
-    order_id = Column(Integer, primary_key=True, index=True)
+    assigned_order_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    order_id = Column(UUID(as_uuid=True), nullable=False, default=uuid.uuid4, index=True)
     status = Column(Enum(OrderStatus), nullable=False, default=OrderStatus.active)
     price = Column(Float, nullable=False)
     executor_id = Column(Integer, nullable=True, index=True)
     zone = Column(String, nullable=False)
     config = Column(JSON, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False, index=True)
+    taken_at = Column(DateTime, nullable=True)
+    updated_at = Column(DateTime, nullable=True)
