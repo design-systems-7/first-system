780b8fc6e5cc5cbc02bd4db5b8764eaa46ab8e13
Blinov Ilya <ulkhan.2014@mail.ru>
2024-11-18 21:48:16 +0300

    Add fallback service

--- backend/app/schemas/requests_config.py
+++ backend/app/schemas/requests_config.py
@@ -19,12 +19,21 @@ class FallbacksConfig(DictConfigBase):
     cache_ttl: NotRequired[int]
     config_data: NotRequired[dict]
     is_fallback_to_config: bool
+    fallback_service_url: NotRequired[str]
+    is_fallback_to_another_service: bool
 
     @field_validator('is_fallback_to_config')
     def config_fallback_configured_properly(cls, v, info: FieldValidationInfo):  # noqa
         if v and "config_data" not in info.data:
             logger.info(info.data)
             raise ValueError('If enabled fallback to config, data in config must be specified')
+        return v
+
+    @field_validator('is_fallback_to_another_service')
+    def fallback_service_configured_properly(cls, v, info: FieldValidationInfo):  # noqa
+        if v and "fallback_service_url" not in info.data:
+            logger.info(info.data)
+            raise ValueError('If enabled fallback to another service, fallback service url must be specified')
         return v
 
 
