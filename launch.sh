903e7fb3edf2513a71e0d37dab78eb7f855499a2
Ilya Kanishev <kanishev2002@gmail.com>
2024-12-17 21:07:21 -0600

    Add launch script and instructions

---
+++ launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd airflow
+
+mkdir -p ./dags ./logs ./plugins ./config
+
+docker compose up airflow-init
+
+cd ..
+
+AIRFLOW_PROJ_DIR=./airflow docker compose -f docker-compose.yml -f airflow/docker-compose.yaml up
