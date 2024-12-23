89cede7afe69c1a1cf5022cc90b5e017e00ee01c
Blinov Ilya <ulkhan.2014@mail.ru>
2024-12-23 11:56:59 +0300

    Clean marts dag

--- airflow/dags/create_marts.py
+++ airflow/dags/create_marts.py
@@ -9,15 +9,17 @@ default_args = {
     "start_date": days_ago(1)
 }
 
+
 def get_last_processed_time():
     return Variable.get("marts_last_processed", default_var="1970-01-01 00:00:00")
+
 
 def update_executor_statistics(**context):
     source_hook = PostgresHook(postgres_conn_id='raw_dwh_layer')
     target_hook = PostgresHook(postgres_conn_id='postgres_marts')
-    
+
     last_processed = context['ti'].xcom_pull(task_ids='get_last_processed_time_task')
-    
+
     sql_query = f"""
         WITH OrderStats AS (
             SELECT 
@@ -40,7 +42,7 @@ def update_executor_statistics(**context):
             accepted_orders_count
         FROM OrderStats
     """
-    
+
     executor_stats = source_hook.get_records(sql_query)
     for executor_stat in executor_stats:
         if executor_stat:
@@ -58,13 +60,14 @@ def update_executor_statistics(**context):
             """
             target_hook.run(insert_sql, parameters=executor_stat)
 
+
 def update_order_snapshots(**context):
     source_hook = PostgresHook(postgres_conn_id='raw_dwh_layer')
     target_hook = PostgresHook(postgres_conn_id='postgres_marts')
-    
+
     last_processed = context['ti'].xcom_pull(task_ids='get_last_processed_time_task')
     current_time = datetime.now()
-    
+
     snapshot_sql = f"""
         WITH current_snapshot AS (
             SELECT 
@@ -86,9 +89,9 @@ def update_order_snapshots(**context):
             total_coins
         FROM current_snapshot 
     """
-    
+
     snapshot_data = source_hook.get_first(snapshot_sql)
-    
+
     if snapshot_data:
         snapshot_insert_sql = """
             INSERT INTO OrderStatisticsSnapshots (
@@ -104,43 +107,19 @@ def update_order_snapshots(**context):
         """
         snapshot_params = (current_time,) + snapshot_data
         target_hook.run(snapshot_insert_sql, parameters=snapshot_params)
-        #
-        # delta_insert_sql = """
-        #     INSERT INTO OrderStatisticsDeltas (
-        #         snapshot_datetime,
-        #         snapshot_datetime_from,
-        #         active_count_delta,
-        #         cancelled_count_delta,
-        #         done_count_delta,
-        #         avg_order_price_in_slice,
-        #         bonus_sum_in_slice,
-        #         total_coins_in_slice
-        #     )
-        #     VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-        # """
-        # delta_params = (
-        #     current_time,
-        #     last_processed,
-        #     snapshot_data[9],   # active_count_delta
-        #     snapshot_data[10],  # cancelled_count_delta
-        #     snapshot_data[11],  # done_count_delta
-        #     snapshot_data[6],   # period_avg_price
-        #     snapshot_data[7],   # period_bonus_sum
-        #     snapshot_data[8]    # period_total_coins
-        # )
-        # target_hook.run(delta_insert_sql, parameters=[delta_params])
+
 
 def update_processed_time(**context):
     current_time = datetime.now()
     Variable.set("marts_last_processed", str(current_time))
+
 
 with DAG(
         'update_marts',
         schedule_interval='*/1 * * * *',
         default_args=default_args,
         catchup=False
-    ) as dag:
-    
+) as dag:
     get_last_processed_time_task = PythonOperator(
         task_id='get_last_processed_time_task',
         python_callable=get_last_processed_time
@@ -164,7 +143,8 @@ with DAG(
         provide_context=True
     )
 
-    get_last_processed_time_task >> [update_executor_statistics_task, update_order_snapshots_task] >> update_processed_time_task
+    get_last_processed_time_task >> [update_executor_statistics_task,
+                                     update_order_snapshots_task] >> update_processed_time_task
 
 if __name__ == "__main__":
     dag.test()
